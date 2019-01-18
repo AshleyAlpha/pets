7440c44c96b274886f21837414522836577aacac
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-01-18 16:09:38 +0200

    JS

--- bank/bank_account.js
+++ bank/bank_account.js
@@ -2,3 +2,6 @@ ox.objectbasics.BankAccount = function (name, balance) {
     this.name = name;
     this.balance = balance;
 }
+ox.objectbasics.BankAccount.prototype.toString = function () {
+    return "Name: " + this.name + "Balance:" + this.balance
+};
