4a6d74af950ff02ec65f5364988e81282b2f3e32
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-01-18 16:18:13 +0200

    JS

--- bank/account.js
+++ bank/account.js
@@ -4,4 +4,6 @@ $(document).ready(function () {
 
     var peter = new ox.objectbasics.BankAccount("Peter", 100);
     console.log(peter.toString());
+    peter.deposit(50);
+    console.log(peter.toString());
 });
--- bank/bank_account.js
+++ bank/bank_account.js
@@ -2,6 +2,16 @@ ox.objectbasics.BankAccount = function (name, balance) {
     this.name = name;
     this.balance = balance;
 }
+
+ox.objectbasics.BankAccount.prototype.deposit = function (amount) {
+    this.balance += amount;
+};
+
+ox.objectbasics.BankAccount.prototype.withdraw = function (amount) {
+    this.balance -= amount;
+};
+
+
 ox.objectbasics.BankAccount.prototype.toString = function () {
     return "Name: " + this.name + "Balance:" + this.balance
 };
