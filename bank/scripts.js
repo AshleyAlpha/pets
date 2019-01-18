275d4a1bc05dc7fc65b768e9b25cd93304777070
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-01-18 15:12:41 +0200

    insertions

--- bank/scripts.js
+++ bank/scripts.js
@@ -9,3 +9,17 @@ function details(full, city, phone, email, id) {
 Contact.prototype.fullName = function () {
     return this.fullName;
 };
+
+// user interface logic
+$(document).ready(function () {
+            $("#hidden1").hide();
+            $("#hidden2").hide();
+            $("form#new-contact").submit(function (event) {
+                        alert(
+                            "You are now done registering. Thank  you for banking with our E-banking system."
+                        );
+                        var inputtedFirstName = $("input#new-full-name").val();
+                        var inputtedLastName = $("input#new-city-name").val();
+                        var inputtedPhone = $("input#new-phone-number").val();
+                        var inputtedEmail = $("input#new-e-mail").val();
+                        var inputtedId = $("input#new-id").val();
