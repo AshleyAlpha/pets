ed7db9419a8755990498bbeb45ce9942f5982337
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-01-19 11:45:48 +0200

    JS

--- bank/bank_account.js
+++ bank/bank_account.js
@@ -8,50 +8,48 @@ var ox = function (name) {
 };
 $(document).ready(function () {
 
-            $("form#detail").submit(function (event) {
-                        event.preventDefault();
-                        var fullName = $("input#fullname").val();
-                        var Name = Name(fullName);
+    $("form#detail").submit(function (event) {
+        event.preventDefault();
+        var fullName = $("input#fullname").val();
+        var Name = Name(fullName);
 
-                        var inputDeposit = parseInt($("input#initial-deposit").val());
-                        $("#user-info").show();
-                        $("form#detail").hide();
-                        $("#user-info h1").text(newName.fullName());
-                        $("#user-info h4").text(newAdress.inputDeposit);
+        var inputDeposit = parseInt($("input#initial-deposit").val());
+        $("#user-info").show();
+        $("form#detail").hide();
+        $("#user-info h1").text(newName.fullName());
+        $("#user-info h4").text(newAdress.inputDeposit);
 
-                        $("#deposit").click(function () {
-                            $("#bank-deposit").toggle();
-                            $("#withdraw").hide();
-                        });
+        $("#deposit").click(function () {
+            $("#bank-deposit").toggle();
+            $("#withdraw").hide();
+        });
 
-                        $("#withdraw").click(function () {
-                            $("#bank-withdraw").toggle();
-                            $("#deposit").hide();
-                        });
+        $("#withdraw").click(function () {
+            $("#bank-withdraw").toggle();
+            $("#deposit").hide();
+        });
 
-                        $("#tdeposit").click(function () {
-                            var depositAmount = parseInt($("input#deposit-amount").val());
-                            var newBalance = inputDeposit + depositAmount;
-                            console.log(newBalance)
+        $("#tdeposit").click(function () {
+            var depositAmount = parseInt($("input#deposit-amount").val());
+            var newBalance = inputDeposit + depositAmount;
+            console.log(newBalance)
 
-                            $("#ac-balance").show();
-                            $("#bank-deposit").hide();
-                            $("#user-info").hide();
-                            $("#demo").text(newBalance)
-                        })
+            $("#ac-balance").show();
+            $("#bank-deposit").hide();
+            $("#user-info").hide();
+            $("#demo").text(newBalance)
+        })
+        $("#withdraw").click(function () {
+            var withdrawAmount = parseInt($("input#withdraw-amount").val());
+            var newBalance = inputDeposit - withdrawAmount;
+            console.log(newBalance)
 
-
-
-
-                        0x.prototype.fullName = function () {
-                            returnthis.balance += amount;
-                        };
-
-                        ox.objectbasics.BankAccount.prototype.withdraw = function (amount) {
-                            this.balance -= amount;
-                        };
+            $("#ac-balance").show();
+            $("#demo").text(newBalance)
+        })
 
 
-                        ox.objectbasics.BankAccount.prototype.toString = function () {
-                            return "Name: " + this.name + "Balance: rwf" + this.balance
-                        };
+    });
+
+
+})
