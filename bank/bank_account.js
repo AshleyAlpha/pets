f14ec5dea1e625b78c82a835963c3eea2c6f20ee
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-01-19 12:22:51 +0200

    Html

--- bank/bank_account.js
+++ bank/bank_account.js
@@ -1,29 +1,49 @@
-var ox = function (name) {
-    this.name = name;
+var newBalance;
 
+function Name(first, last) {
+    this.first = first;
+    this.last = last;
 }
 
-0x.prototype.fullName = function () {
-    return this.name;
-};
+function Adresses(email, number, deposit) {
+    this.inputEmail = email;
+    this.inputNumber = number;
+    this.inputDeposit = deposit;
+}
+
+Name.prototype.fullName = function () {
+    return this.first + " " + this.last;
+}
+
 $(document).ready(function () {
 
-    $("form#detail").submit(function (event) {
+    $("form#contact").submit(function (event) {
         event.preventDefault();
-        var fullName = $("input#fullname").val();
-        var Name = Name(fullName);
+        var firstName = $("input#firstname").val();
+        var lastName = $("input#lastname").val();
+        var newName = new Name(firstName, lastName);
 
+        var inputEmail = $("input#e-mail").val();
+        var inputNumber = $("input#p-number").val();
         var inputDeposit = parseInt($("input#initial-deposit").val());
-        $("#user-info").show();
-        $("form#detail").hide();
-        $("#user-info h1").text(newName.fullName());
-        $("#user-info h4").text(newAdress.inputDeposit);
+        var newAdress = new Adresses(inputEmail, inputNumber, inputDeposit);
 
+
+
+        //gether user info and hide form    
+        $("#user-info").show();
+        $("form#contact").hide();
+        $("#user-info h1").text(newName.fullName());
+        $("#user-info h2").text(newAdress.inputEmail);
+        $("#user-info h3").text(newAdress.inputNumber);
+        $("#user-info h4").text(newAdress.inputDeposit);
+        //open deposit form
         $("#deposit").click(function () {
+            // $("button#deposit").hide();
             $("#bank-deposit").toggle();
             $("#withdraw").hide();
         });
-
+        //open withdraw form
         $("#withdraw").click(function () {
             $("#bank-withdraw").toggle();
             $("#deposit").hide();
@@ -39,7 +59,7 @@ $(document).ready(function () {
             $("#user-info").hide();
             $("#demo").text(newBalance)
         })
-        $("#withdraw").click(function () {
+        $("#twithdraw").click(function () {
             var withdrawAmount = parseInt($("input#withdraw-amount").val());
             var newBalance = inputDeposit - withdrawAmount;
             console.log(newBalance)
