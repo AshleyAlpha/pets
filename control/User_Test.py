42d1efa4d0d1677cf6bb69c37624dc41781a1abc
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-02-18 10:12:24 +0200

    created class

--- control/User_Test.py
+++ control/User_Test.py
@@ -1,1 +1,11 @@
- 
+import unittest # Importing the unittest module
+from users import Users # Importing the contact class
+
+class TestUsers(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the Users class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self): 
