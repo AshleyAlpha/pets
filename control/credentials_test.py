90c30b97ee5c4fc0d09f2424155bed04eb87d4fc
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-02-18 09:43:58 +0200

    insertion of more tests

--- control/credentials_test.py
+++ control/credentials_test.py
@@ -52,3 +52,46 @@ def test_save_multiple_credential(self):
             test_credential = Credentials("facebook","miahmamie") # new credential
             test_credential.save_credential()
             self.assertEqual(len(Credentials.credentials_list),2)
+            # More tests above
+    def test_delete_credential(self):
+            '''
+            test_delete_credential to test if we can remove a credential from our credentials list
+            '''
+            self.new_credential.save_credential()
+            test_credential = Credentials("facebook","miahmamie") # new credential
+            test_credential.save_credential()
+
+            self.new_credential.delete_credential()# Deleting a credential  object
+            self.assertEqual(len(Credentials.credentials_list),1)
+
+    def test_find_credential_by_app_name(self):
+        '''
+        test to check if we can find a credential by app_name and display information
+        '''
+
+        self.new_credential.save_credential()
+        test_credential = Credentials("facebook","miahmamie") # new credential
+        test_credential.save_credential()
+
+        found_credential = Credentials.find_by_app_name("facebook")
+
+        self.assertEqual(found_credential.app_name,test_credential.app_name)
+
+    def test_credential_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the credential.
+        '''
+
+        self.new_credential.save_credential()
+        test_credential = Credentials("facebook","miahmamie") # new credential
+        test_credential.save_credential()
+
+        credential_exists = Credentials.credential_exist("faceook")
+
+        self.assertFalse(credential_exists)
+    def test_display_all_credential(self):
+        '''
+        method that returns a list of all credentials saved
+        '''
+
+        self.assertEqual(Credentials.display_credential(),Credentials.credentials_list)
