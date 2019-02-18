49a69c2da09499bcff1302be952292560156a346
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-02-18 09:54:46 +0200

    saving new credential & deletion of user

--- control/run.py
+++ control/run.py
@@ -6,10 +6,27 @@ def create_user(email,username,password):
     '''
     new_user = Users(email,username,password)
     return new_user
-    
+
     def create_credential(app_name,password):
     '''
     Function to create a new credential
     '''
     new_credential = Credentials(app_name,password)
     return new_credential
+def save_user(user):
+    '''
+    Function to save new user
+    '''
+    user.save_user()
+
+def save_credential(credential):
+    '''
+    Function to save new credential
+    '''
+    credential.save_credential()
+
+def del_user(user):
+    '''
+    Function to delete a user
+    '''
+    user.delete_user()
