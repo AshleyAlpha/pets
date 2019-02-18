b13e6ff68a8653d79af92056809aba39cec3754f
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-02-18 09:27:28 +0200

    saving credentials

--- control/credentials.py
+++ control/credentials.py
@@ -4,3 +4,8 @@ class Credentials:
 
         self.app_name = app_name
         self.password = password
+        def save_credential(self):
+        '''
+        save_credential method saves credential objects into credentials_list
+        '''
+        Credentials.credentials_list.append(self)
