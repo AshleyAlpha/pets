acf31eb42531ccead97b0d9acc06d8551edefece
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-02-18 10:01:23 +0200

    main insertion

--- control/run.py
+++ control/run.py
@@ -74,3 +74,35 @@ def display_credential():
 def main():
     print("Hello Welcome to your users list. What is your name?")
     user_name = input()
+    print(f"Hello {user_name}. what would you like to do?")
+    print('\n')
+
+    print("New user")
+    print("-"*10)
+
+    print ("email ....")
+    email = input()
+
+    print("username ...")
+    username = input()
+
+    print("password ...")
+    password = input()
+
+    print("now let procceed to login to our account")
+    print('\n')
+
+    print("enter your username (the username must be the same as the first username you entered previously ):")
+    print('\n')
+    print("enter username")
+    login_name=input()
+
+    print("enter password")
+    passw=input()
+    
+    if password==passw and username==login_name:
+        print("successfully logged in")
+        print('\n')
+    else:
+        print(f"password: {passw} or name: {login_name} incorrect. Next time , Please enter password correctly.")  
+        sys.exit()
