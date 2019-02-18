367c0f5624e2c46f8dba0b7810214f94b98c5745
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-02-18 10:17:10 +0200

    setup method insertions

--- control/User_Test.py
+++ control/User_Test.py
@@ -8,4 +8,51 @@ class TestUsers(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
-    def setUp(self): 
+    def setUp(self):
+         #setup method to run before each test cases.
+        
+        self.new_user = Users("mashleyalpha@gmail.com","ashleyalpha","miahmamie") # create contact object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.email,"mashleyalpha@gmail.com")
+        self.assertEqual(self.new_user.username,"ashleyalpha")
+        self.assertEqual(self.new_user.password,"miahmamie")
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the users list
+        '''
+        self.new_user.save_user() # saving the new users
+        self.assertEqual(len(Users.users_list),1)
+    
+    def test_save_multiple_user(self):
+            '''
+            test_save_multiple_user to check if we can save multiple user
+            objects to our users_list
+            '''
+            self.new_user.save_user()
+            test_user = Users("mashleyalpha@gmail.com","ashleyalpha","miahmamie") # new user
+            test_user.save_user()
+            self.assertEqual(len(Users.users_list),2)
+#setup and class creation up here
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            Users.users_list = []
+
+# other test cases here
+    def test_save_multiple_user(self):
+            '''
+            test_save_multiple_user to check if we can save multiple user
+            objects to our users_list
+            '''
+            self.new_user.save_user()
+            test_user = Users("mashleyalpha@gmail.com","ashleyalpha","miahmamie") # new user
+            test_user.save_user()
+            self.assertEqual(len(Users.users_list),2)
