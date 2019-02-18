8eae731ebd0676a17ea8369e663517d02821421e
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-02-18 10:32:43 +0200

    finding the username

--- control/User.py
+++ control/User.py
@@ -12,3 +12,16 @@ class Users:
         save_user method saves user objects into users_list
         '''
         Users.users_list.append(self)
+    @classmethod
+    def find_by_username(cls,username):
+        '''
+        Method that takes in a username and returns a user that matches that username.
+        Args:
+            username: username to search for
+        Returns :
+            username of person that matches the username.
+        '''
+
+        for user in cls.users_list:
+            if user.username == username:
+                return user
