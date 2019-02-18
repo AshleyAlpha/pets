ce385d80516a5da3f0b51c75e36510a2da0efd0b
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-02-18 09:29:19 +0200

    class method insertion to find app name

--- control/credentials.py
+++ control/credentials.py
@@ -9,3 +9,23 @@ class Credentials:
         save_credential method saves credential objects into credentials_list
         '''
         Credentials.credentials_list.append(self)
+         def delete_credential(self):
+
+        '''
+        delete_credential method deletes a saved credential from the credentials_list
+        '''
+
+        Credentials.credentials_list.remove(self)
+@classmethod
+    def find_by_app_name(cls,app_name):
+        '''
+        Method that takes in an app_name and returns a credential that matches that app_name.
+        Args:
+            app_name: app_name to search for
+        Returns :
+            app_name of credential that matches the app_name.
+        '''
+
+        for credential in cls.credentials_list:
+            if credential.app_name == app_name:
+                return credential
