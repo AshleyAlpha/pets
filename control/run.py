eba22adac6b75cacb3bff6e4e517ef82395558de
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-02-18 09:56:15 +0200

    insertion for finding,checking, deleting

--- control/run.py
+++ control/run.py
@@ -30,3 +30,26 @@ def del_user(user):
     Function to delete a user
     '''
     user.delete_user()
+def del_credential(credential):
+    '''
+    Function to delete a credential
+    '''
+    credential.delete_credential()
+
+def find_user(username):
+    '''
+    Function that finds a user by username and returns the user
+    '''
+    return user.find_by_username(username)
+
+def find_credential(app_name):
+    '''
+    Function that finds a credential by app_name and returns the credential
+    '''
+    return Credentials.find_by_app_name(app_name)
+
+def check_existing_user(username):
+    '''
+    Function that check if a user exists with that username and return a Boolean
+    '''
+    return Users.user_exist(username)
