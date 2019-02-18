cc92ef1db40927d3e8d1534c7d8f29cb3feaa15f
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-02-18 09:50:04 +0200

    created user info

--- control/run.py
+++ control/run.py
@@ -1,2 +1,15 @@
 from user import User
 from credentials import Credentials
+def create_user(email,username,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = Users(email,username,password)
+    return new_user
+    
+    def create_credential(app_name,password):
+    '''
+    Function to create a new credential
+    '''
+    new_credential = Credentials(app_name,password)
+    return new_credential
