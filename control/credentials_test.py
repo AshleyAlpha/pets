3811b385534ebe877b69f2948ae085748a2573eb
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-02-18 09:35:08 +0200

    test.init

--- control/credentials_test.py
+++ control/credentials_test.py
@@ -1,2 +1,14 @@
 import unittest # Importing the unittest module
 from credentials import Credentials # Importing the contact class
+class TestCredentials(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+    Args:
+    unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_credential = Credentials("facebook","miahmamie") # create credential object
