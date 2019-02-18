e917dfcb0a32e1bcc5f311ebca694784bd6e9b83
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-02-18 10:29:12 +0200

    saving user

--- control/User.py
+++ control/User.py
@@ -6,3 +6,9 @@ class Users:
         self.email = email
         self.username = username
         self.password = password
+
+    def save_user(self):
+        '''
+        save_user method saves user objects into users_list
+        '''
+        Users.users_list.append(self)
