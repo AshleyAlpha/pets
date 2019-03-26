06571213366f001fc9c10e35fa2c8ef01ceec478
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-03-26 17:26:37 +0200

    url modified

--- ig/instag/urls.py
+++ ig/instag/urls.py
@@ -1,7 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.cheznous, name='cheznous'),
-    
+    url(r'^$',views.cheznous, name='nous'),
+    url(r'^profile/', views.profile, name='profile'),
+    url(r'^theProfile/(\d+)', views.theProfile, name='theProfile'),
+    url(r'^picture/', views.picture, name='picture'),
+    url(r'^images/(\d+)',views.images,name ='images'),
+
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
